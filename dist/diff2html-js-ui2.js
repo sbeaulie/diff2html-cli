97be01ad2a33b5110b2460303d306e0b825df0cf
Samuel Beaulieu <samuel@puppetlabs.com>
2018-09-26 18:19:32 -0500

    Move the script block into it's own .js file for the server security would not load the script block

---
+++ dist/diff2html-js-ui2.js
@@ -0,0 +1,6 @@
+    $(document).ready(function() {
+      var diff2htmlUi = new Diff2HtmlUI();
+      //diff2html-fileListCloseable
+      //diff2html-synchronisedScroll
+      diff2htmlUi.highlightCode('#diff');
+    });
